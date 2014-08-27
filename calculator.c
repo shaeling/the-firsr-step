2e64516c2354d72c8cef7d06c8ca5f2e1e9f19e1
shaeling <ying_slaying@sina.cn>
2014-08-27 10:33:58 +0800

    Create calculator.c

---
+++ calculator.c
@@ -0,0 +1,16 @@
+#i dont know the grammer in C,so i just public some code and you can change it.And if you try to use Sublime Text 2,
+  some driver for C is needed.then have a good luck in coding~
+#this is a calculator test file
+
+import "stdio.h"
+
+void main(){
+  A = raw_input("please enter a number a:",&d)
+  B = raw_inpue("please enter a number b:",&d)
+  
+  sum = A + b
+  
+  printf(sum,%d)
+  
+
+}
